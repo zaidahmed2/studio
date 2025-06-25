11d5fe64fce9bca7835070af4072651b30c1f578
Zaid Ahmed <zaidmeer214@gmail.com>
2025-06-25 04:22:58 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ZaniaLogo classNa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Great+Vibes&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Dancing+Script:wght@700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         <div className="animated-bg">
--- src/components/zania-logo.tsx
+++ src/components/zania-logo.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils"
 export function ZaniaLogo({ className, ...props }: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      viewBox="0 0 150 50"
+      viewBox="0 0 160 60"
       xmlns="http://www.w3.org/2000/svg"
       className={cn("w-auto", className)}
       {...props}
@@ -12,10 +12,10 @@ export function ZaniaLogo({ className, ...props }: React.SVGProps<SVGSVGElement>
       <title>Zania Logo</title>
       <text
         x="5"
-        y="40"
-        fontFamily="'Great Vibes', cursive"
-        fontSize="48"
-        fontWeight="400"
+        y="45"
+        fontFamily="'Dancing Script', cursive"
+        fontSize="52"
+        fontWeight="700"
         fill="currentColor"
       >
         Zania
