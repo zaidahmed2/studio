228d2cf9184048a00de658f775eba2d55cd95383
Zaid Ahmed <zaidmeer214@gmail.com>
2025-06-25 04:21:39 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ZaniaLogo classNa

--- src/components/zania-logo.tsx
+++ src/components/zania-logo.tsx
@@ -4,18 +4,18 @@ import { cn } from "@/lib/utils"
 export function ZaniaLogo({ className, ...props }: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
-      viewBox="0 0 90 40"
+      viewBox="0 0 150 50"
       xmlns="http://www.w3.org/2000/svg"
       className={cn("w-auto", className)}
       {...props}
     >
       <title>Zania Logo</title>
       <text
-        x="0"
-        y="32"
-        fontFamily="Poppins, sans-serif"
-        fontSize="36"
-        fontWeight="600"
+        x="5"
+        y="40"
+        fontFamily="'Great Vibes', cursive"
+        fontSize="48"
+        fontWeight="400"
         fill="currentColor"
       >
         Zania
